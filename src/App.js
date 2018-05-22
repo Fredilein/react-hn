321567ef6f9761e8250a8f42aeeb42525d75cc1b
Adi <azanga@gmx.ch>
2018-05-22 23:06:41 +0200

    get stories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = { 
+      titles: [] 
+    };
+  }
+
+  async getStories(topstories) {
+    
+    const topstories_ids = await axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json`);
+
+    for (let i = 0; i < 10; i++) {
+      axios.get(`https://hacker-news.firebaseio.com/v0/item/${topstories_ids.data[i]}.json`)
+      .then(res => {
+        const titles = this.state.titles.slice();
+        titles[i] = res.data.title;
+        this.setState({titles: titles})
+      });
+    }
+
+    
+  }
+
+  componentDidMount() {
+    this.getStories();
+  }
+  
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +43,18 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <ul>
+          {this.state.titles.map(title =>
+            <li key={title}>{title}</li>
+          )}
+        </ul>
+        </div>
       </div>
     );
   }
 }
+
+
 
 export default App;
