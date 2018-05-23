e8bf7466120081a6893f689eb8ec4bdf0a80b6e7
Adi <azanga@gmx.ch>
2018-05-23 23:56:01 +0200

    clock and style

--- src/App.js
+++ src/App.js
@@ -1,111 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
+import Hackernews from './Components/Hackernews.js';
+import Clock from './Components/Clock.js';
+import Background from './background.jpg';
 
 
-class Story extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.MAX_URL_LENGTH = 50;
-    
-    this.state = {
-      title: '',
-      url: '',
-      score: 0,
-    };
-  }
+class App extends React.Component {
 
-  componentDidMount() {
-    axios.get(`https://hacker-news.firebaseio.com/v0/item/${this.props.id}.json`)
-    .then(res => {
-      this.setState({
-        title: res.data.title,
-        url: res.data.url,
-        score: res.data.score
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  render() {
-    const url = this.state.url;
-    const title = this.state.title;
-    const score = this.state.score;
-
-    const url_cropped = url.substring(0, this.MAX_URL_LENGTH);
-    const url_display = (url > url_cropped ? url_cropped+"..." : url_cropped);
-
-    
-    return(      
-      <a class="list-group-item" href={url} target="_blank">
-        <div class="row">
-          <div class="col-xs-1">
-            <h4>{score}</h4>
-          </div>
-          <div class="col-xs-11">
-            <h4 class="list-group-item-heading">{title}</h4>
-            <p class="list-group-item-text">{url_display}</p>
-          </div>
-        </div>
-      </a>
-    );
-  }
-
-}
-
-
-class Hackernews extends React.Component {
-  
-  constructor() {
-    super();
-    this.state = { 
-      stories: [],
-    };
-  }
-
-  componentDidMount() {
-    axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json`)
-    .then(res => {
-      const ids = res.data.slice(0, 10);
-      this.setState({stories: ids});
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  render() {    
-    return (
-      <div class="col-md-6 col-md-offset-3">
-        <div class="list-group">
-          {this.state.stories.map(id =>
-            <Story id={id} key={id} />
-          )}   
-        </div>
-      </div>
-    );
-  }
-}
-
-
-class App extends Component {
-  
-  render() {
+  render() {		
+		var appStyle = {
+			backgroundImage: `url(${Background})`,
+		};
 
     return (
-      <div className="App">
+      <div className="App" style={appStyle}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Clock />
+          <h1 className="lead App-title"><em>Home Sweet Home.</em></h1>
         </header>
-        <p className="App-intro">
-          HackerNews Articles
-        </p>
-        <div class="container">
+        <div className="container">
           <Hackernews />
         </div>
       </div>
---
+++ src/Components/Clock.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Clock extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <h1>{this.state.date.toLocaleTimeString()}</h1>
+    );
+  }
+}
+
+export default Clock;
